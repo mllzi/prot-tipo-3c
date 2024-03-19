569cbfdb5b47ac31039c7833a2df6f614065c23b
bia <bianca.calixto.soares@escola.pr.gov.br>
2024-03-19 11:01:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
+//laço de repetição
+
 for(let i=0; i < botoes.length; i++) {
  botoes[i].onclick = function(){
 
-   for(let j=0;botoes.length;j++){
+   for(let j=0; j < botoes.length;j++){
        botoes[j].classList.remove("ativo");
        textos[j].classList.remove("ativo");  
     }
@@ -13,3 +15,15 @@ for(let i=0; i < botoes.length; i++) {
    textos[i].classList.add("ativo");
   }
 }
+
+const contadores = document.querySelectorAll(".contador"); 
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+  function calculaTempo(tempoObjetivo1) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+  }
