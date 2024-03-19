58ea13289b1ebfc58a32cf30a329dcf5cf2185fc
bia <bianca.calixto.soares@escola.pr.gov.br>
2024-03-19 09:57:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0; i < botoes.length; i++) {
  botoes[i].onclick = function(){
 
    for(let j=0;botoes.length;j++){
        botoes[j].classList.remove("ativo");
-   }
-   botoes[j].classList.add("ativo");
-}
+       textos[j].classList.remove("ativo");  
+    }
+   botoes[i].classList.add("ativo");
+   textos[i].classList.add("ativo");
+  }
 }
