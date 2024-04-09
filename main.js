0f41511de33bf1bdfd6cf4aa617963b4a6b89964
bia <bianca.calixto.soares@escola.pr.gov.br>
2024-04-09 12:35:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
 const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 
   function calculaTempo(tempoObjetivo1) {
@@ -42,3 +42,19 @@ const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
   return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 
 }
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function atualizaCronometro(){
+ 
+ for(let i=0; i<contadores.length;i++){
+  contadores[i].textContent = calculaTempo(tempos[i]);
+ }
+
+}
+
+function comecaCronometro(){
+  atualizaCronometro();
+  setInterval(atualizaCronometro,1000);
+}
+comecaCronometro();
