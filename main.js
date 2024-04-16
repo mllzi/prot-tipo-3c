ec5f03ff792f9a31d3a118fe191715052453aa06
bia <bianca.calixto.soares@escola.pr.gov.br>
2024-04-16 11:58:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
@@ -17,10 +16,10 @@ for(let i=0; i < botoes.length; i++) {
 }
 const contadores = document.querySelectorAll(".contador"); 
 
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-05-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-09-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-06-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-05T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
@@ -39,11 +38,18 @@ const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
   minutos %= 60;
   horas %= 24;
 
-  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+if (tempoFinal > 0){
+      return [dias,horas,minutos,segundos];
+} else {
+  return[0,0,0,0];
+}
+
 
 }
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+//return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+
+contadores[0].textContent = calculaTempo(tempoObjetivo);
 
 function atualizaCronometro(){
  
